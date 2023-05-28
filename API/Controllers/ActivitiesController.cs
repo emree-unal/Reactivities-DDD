08741367df0790e983bf0ed13878f20b86a44969
emreeuunal@gmail.com <emreeuunal@gmail.com>
2023-05-28 15:36:09 +0300

    Add project files.

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,31 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+
+  public class ActivitiesController : BaseApiController
+  {
+    private readonly DataContext _contex;
+
+    public ActivitiesController(DataContext contex)
+    {
+      _contex = contex;
+    }
+
+    [HttpGet("getAllActivities")]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+      return await _contex.Activities.ToListAsync();
+    }
+
+    [HttpGet("getActivityById")]
+    public async Task<ActionResult<Activity>> GetActivity(Guid id)
+    {
+      return await _contex.Activities.FirstAsync(x=>x.Id == id);
+    }
+  }
+}
